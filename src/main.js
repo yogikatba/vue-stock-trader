7791e85fdda6b1fe99dca1d68950cb9a2d29f886
yogikatba <yogikatba16@gmail.com>
2020-07-14 01:41:02 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+import App from './App.vue'
+import {
+  routes
+} from './routes';
+import store from './store/store';
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vue-stock-trade-50bc3.firebaseio.com/';
+
+Vue.filter('currency', (value) => {
+  return '$' + value.toLocaleString();
+});
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+});
+
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App)
+})
